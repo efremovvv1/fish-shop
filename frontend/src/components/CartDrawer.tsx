a05750ec1077ded22cc7f2e265b92d7e2bdb4165
efremovvv1 <145122434+efremovvv1@users.noreply.github.com>
2026-04-03 18:19:42 +0200

    Basket Img

--- frontend/src/components/CartDrawer.tsx
+++ frontend/src/components/CartDrawer.tsx
@@ -81,7 +81,7 @@ export default function CartDrawer({ onCheckout }: Props) {
               justifyContent: "center",
             }}
           >
-            {collapsed ? "▴" : "▾"}
+            {collapsed ? "🧺" : "▾"}
           </button>
         </div>
       </div>
